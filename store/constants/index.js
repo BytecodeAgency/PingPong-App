899c89459fa162beb5c830d287418cc49fd5cef2
Jeroen van Steijn <j.j.vansteijn@student.tudelft.nl>
2019-05-31 11:20:41 +0200

    Empty repo

--- store/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const HANDLE_LOGIN = 'HANDLE_LOGIN';
-export const HANDLE_REGISTER = 'HANDLE_REGISTER';
-
-export const ADD_GAME = 'ADD_GAME';
-
-export const ADD_ERROR = 'ADD_ERROR';
-export const CLEAR_ERRORS = 'CLEAR_ERRORS';
--- store/reducers/navigator.js
+++
@@ -1,7 +0,0 @@
-import { createNavigationReducer } from 'react-navigation-redux-helpers';
-
-import AppNavigator from '../../routes';
-
-const navReducer = createNavigationReducer(AppNavigator);
-
-export default navReducer;
